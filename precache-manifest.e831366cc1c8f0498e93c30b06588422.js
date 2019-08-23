5f9998e87451a7f4beda5d690e5efdc269100b55
JineHyun <rjs5967@naver.com>
2019-08-23 22:24:13 +0900

    Updates

---
+++ precache-manifest.e831366cc1c8f0498e93c30b06588422.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9991136b994266e3f6e9a832523dcf0f",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "167b02de6a1cdbe644c9",
+    "url": "/movie_app/static/css/main.c4f92bf2.chunk.css"
+  },
+  {
+    "revision": "ad63ab08a2a8e5504836",
+    "url": "/movie_app/static/js/2.7b834e7d.chunk.js"
+  },
+  {
+    "revision": "167b02de6a1cdbe644c9",
+    "url": "/movie_app/static/js/main.77f1e4c6.chunk.js"
+  },
+  {
+    "revision": "c6514544d436775abdc2",
+    "url": "/movie_app/static/js/runtime~main.175dda75.js"
+  }
+]);
